247f4f4b3ad8ea3a02138828f77b93e280f64946
Brian Lederman <brian.j.lederman@gmail.com>
2014-05-03 19:50:59 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HiartSplash::Application.config.secret_key_base = '6c65fcfe3f9ebb343d175ef7486f6a06c3b4197c2451752014daab0a07c591ca6be4881fb34b12c2db11145933e0e1236eeee8144c0eff3e2fb822c13733f56c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HiartSplash::Application.config.session_store :cookie_store, key: '_hiart_splash_session'
