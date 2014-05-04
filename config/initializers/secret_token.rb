f8715cc7d5256d7a7d26dc0d7beea22b2e84dfee
Brian Lederman <brian.j.lederman@gmail.com>
2014-05-03 21:00:36 -0400

    Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 HiartSplash::Application.config.secret_key_base = '6c65fcfe3f9ebb343d175ef7486f6a06c3b4197c2451752014daab0a07c591ca6be4881fb34b12c2db11145933e0e1236eeee8144c0eff3e2fb822c13733f56c'
