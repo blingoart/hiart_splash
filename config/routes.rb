ef1053b2947d99bda5f6cd68f6007a9590cd84c1
Brian Lederman <brian.j.lederman@gmail.com>
2014-05-05 00:41:32 -0400

    Updated Newsletter

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 HiartSplash::Application.routes.draw do
+ 
+  resources :users
   root "pages#home"
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
